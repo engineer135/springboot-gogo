b6ce76e37a2e65ec5737d66cf5a494f150d62404
engineer135 <everafterk@naver.com>
2021-03-19 17:29:44 +0900

    누락 파일 커밋

--- src/main/java/com/springboot/gogo/first/Application.java
+++ src/main/java/com/springboot/gogo/first/Application.java
@@ -1,9 +1,9 @@
-package com.springboot-gogo.first;
+package com.springboot.gogo.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApllication
+@SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
--- src/main/java/com/springboot/gogo/first/web/HelloController.java
+++ src/main/java/com/springboot/gogo/first/web/HelloController.java
@@ -1,2 +1,20 @@
-package com.springboot.gogo.first.web;public class HelloController {
+package com.springboot.gogo.first.web;
+
+import com.springboot.gogo.first.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 }
