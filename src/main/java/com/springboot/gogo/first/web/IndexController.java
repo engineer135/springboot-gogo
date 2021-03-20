2fdc564a7b4afb90bcf275b17aeb6af90bcd4e32
engineer135 <everafterk@naver.com>
2021-03-20 18:52:45 +0900

    로그인 사용자 정보 어노테이션 등록

--- src/main/java/com/springboot/gogo/first/web/IndexController.java
+++ src/main/java/com/springboot/gogo/first/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.gogo.first.web;
 
+import com.springboot.gogo.first.config.auth.LoginUser;
 import com.springboot.gogo.first.config.auth.dto.SessionUser;
 import com.springboot.gogo.first.service.posts.PostsService;
 import com.springboot.gogo.first.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
