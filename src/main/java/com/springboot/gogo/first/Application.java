3db390565bb71981241748f2f864d3b1e037f5c6
engineer135 <everafterk@naver.com>
2021-03-21 11:33:00 +0900

    스프링 시큐리티 사용으로 인한 테스트 코드 문제 해결!

--- src/main/java/com/springboot/gogo/first/Application.java
+++ src/main/java/com/springboot/gogo/first/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // jpa auditing 활성화
+//@EnableJpaAuditing // jpa auditing 활성화 > 테스트를 위해 분리 처리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
