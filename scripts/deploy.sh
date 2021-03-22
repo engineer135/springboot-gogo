2295694f18602bf8c7a66d54330a2d347a20cefb
engineer135 <everafterk@naver.com>
2021-03-22 18:02:03 +0900

    오타 수정 -_-

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,15 +9,15 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRET_PID=$(pgrep -fl springboot-gogo | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-gogo | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid : $CURRET_PID"
 
 if [ -z "$CURRENT_PID"]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRET_PID"
-    kill -15 $CURRET_PID
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
 
