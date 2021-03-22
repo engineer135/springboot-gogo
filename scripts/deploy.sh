1a99cb33c1b9ca7f9cda0ea1459a0ea6d01968ea
engineer135 <everafterk@naver.com>
2021-03-22 13:28:16 +0900

    deploy.sh 오타 수정 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME = $(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> jar name : $JAR_NAME"
 echo "> $JAR_NAME에 실행권한 추가"
